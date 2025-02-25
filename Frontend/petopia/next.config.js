cad34690bf16e2e8ad2ab827114552082eb5a1a2
Gluko <gluko.official@gmail.com>
2025-02-25 12:21:35 +0200

    Update requirements and image configuration for improved environment handling

--- Frontend/petopia/next.config.js
+++ Frontend/petopia/next.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   images: {
-    domains: ['127.0.0.1', 'localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http', // Change to 'https' if required
+        hostname: '127.0.0.1',
+      },
+      {
+        protocol: 'http', // Change to 'https' if required
+        hostname: 'localhost',
+      },
+    ],
   },
 }
